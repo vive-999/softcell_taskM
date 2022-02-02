d25ce02061c26cb6712c50ac407e9bc159557d26
Vivek Raj <raj.vivek999@gmail.com>
2022-02-02 11:07:43 +0530

    Added object name so that name shows in admin panel rather than object number

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
  - Please Place your Templates(html) file under the apps template folder(task/templates/task/*.html)
  - Place your static files(CSS) under the static folder in the app(task/static/task/*.css)
  - Place your images for the apps under the images directory in static(task/static/task/*.jpg)
+
--- task/models.py
+++ task/models.py
@@ -9,9 +9,16 @@ class Group(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 
 class User(models.Model):
     email = models.EmailField()
@@ -19,6 +26,9 @@ class User(models.Model):
     group_member = models.ForeignKey(Group, on_delete=models.CASCADE)
     project_member = models.ForeignKey(Project, on_delete=models.CASCADE)
     role = models.CharField(max_length=30, default=None)
+
+    def __str__(self):
+        return self.email
 
 class Incident(models.Model):
     date = models.DateField()
@@ -29,6 +39,9 @@ class Incident(models.Model):
     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
     service_group = models.ForeignKey(Group,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.issue_subject
+
 class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
@@ -38,5 +51,8 @@ class Task(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
     project_id = models.ForeignKey(Project,on_delete=models.CASCADE)
     group_id = models.ForeignKey(Group,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
     
 
